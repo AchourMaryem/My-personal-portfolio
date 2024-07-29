ba731856149628be87fb1d4c677b66b290e6e0fe
AchourMariem <mariem.achour@polytechnicien.tn>
2024-07-29 11:31:24 +0100

    update image

--- components/Avatar.js
+++ components/Avatar.js
@@ -2,16 +2,19 @@ import React from 'react';
 import Image from 'next/image';
 
 const Avatar = ({ width, height }) => {
-  const defaultWidth = 200; // Définissez la largeur par défaut ici
+  const defaultWidth = 200; 
+  const defaultHeight = 200; 
 
   return (
     <div className='hidden xl:flex xl:max-w-none'>
       <Image
-        src="mariem.png"
+        src="/mariem.png"
         width={width || defaultWidth}
-        height={height}
-        alt=""
+        height={height || defaultHeight}
+        alt="Avatar de Mariem"
         className='translate-z-0 w-full h-full'
+        layout='intrinsic' 
+        quality={100}
       />
     </div>
   );
